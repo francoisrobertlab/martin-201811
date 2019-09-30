42cd4c1baf2d4cabf16ca39888a9ca3cc09c3d3a
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-09-30 15:38:38 -0400

    Use seqtools to limit code duplication.

--- mnaseseqtools/FullAnalysis.py
+++ mnaseseqtools/FullAnalysis.py
@@ -1,15 +1,15 @@
 import logging
-from multiprocessing import Pool
 import os
 
-import AlignSample
-import BamToBed
-import DownloadSample
-import FilterBam
-import GenomeCoverage
-import MergeSampleBed
-import SplitBed
+import PrepareGenomeCoverage
 import click
+import pandas as pd
+import seqtools.Bam2Bed
+import seqtools.DownloadSample
+import seqtools.GenomeCoverage
+import seqtools.MergeSampleBed
+import seqtools.RunBwa
+import seqtools.SplitBed
 
 
 @click.command()
@@ -24,52 +24,28 @@ import click
               help='Size of chromosomes.')
 @click.option('--threads', '-t', default=1,
               help='Number of threads used to process data per sample.')
-@click.option('--poolThreads', '-T', default=2,
-              help='Samples to process in parallel.')
 @click.option('--splitLength', type=int, default=None,
               help='Split reads in bins by their length.')
 @click.option('--splitMinLength', default=100,
               help='Split reads minimum length.')
 @click.option('--splitMaxLength', default=500,
               help='Split reads maximum length.')
-def main(samples, merge, fasta, sizes, threads, poolthreads, splitlength, splitminlength, splitmaxlength):
+def main(samples, merge, fasta, sizes, threads, splitlength, splitminlength, splitmaxlength):
     '''Analyse Martin et al. data from November 2018 in Genetics.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    AlignSample.bwa_index(fasta)
-    samples_columns = all_columns(samples)
-    samples_pool_args = []
-    for sample_columns in samples_columns:
-        sample = sample_columns[0]
-        fastq = sample_columns[1] if len(sample_columns) > 1 else None
-        srr = sample_columns[2] if len(sample_columns) > 2 else None
-        samples_pool_args.append((sample, fastq, srr, fasta, sizes, splitlength, splitminlength, splitmaxlength, threads))
-    with Pool(poolthreads) as pool:
-        pool.starmap(analyse, samples_pool_args)
-    merges_columns = all_columns(merge) if os.path.isfile(merge) else []
-    merges_pool_args = []
-    for merge_columns in merges_columns:
-        sample = merge_columns[0]
-        samples_to_merge = merge_columns[1:] if len(merge_columns) > 1 else None
+    RunBwa.bwa_index(fasta)
+    sample_columns = pd.read_csv(samples, header=None, sep='\t', comment='#')
+    for index, columns in sample_columns.iterrows():
+        sample = columns[0]
+        fastq = columns[1] if len(columns) > 1 else None
+        srr = columns[2] if len(columns) > 2 else None
+        analyse(sample, fastq, srr, fasta, sizes, splitlength, splitminlength, splitmaxlength, threads)
+    merge_columns = pd.read_csv(merge, header=None, sep='\t', comment='#')
+    for index, columns in merge_columns.iterrows():
+        sample = columns[0]
+        samples_to_merge = columns[1:] if len(columns) > 1 else None
         MergeSampleBed.merge_samples(sample, samples_to_merge)
-        merges_pool_args.append((sample, sizes, splitlength, splitminlength, splitmaxlength))
-    with Pool(poolthreads) as pool:
-        pool.starmap(analyse_merged, merges_pool_args)
-
-
-def all_columns(file):
-    all = []
-    with open(file, 'r') as lines:
-        for line in lines:
-            if line.startswith('#'):
-                continue
-            columns = line.rstrip('\n\r').split('\t');
-            all.extend([columns])
-    return all
-    
-    
-def first_column(file):
-    all = all_columns(file)
-    return [columns[0] for columns in all]
+        analyse_merged(sample, sizes, splitlength, splitminlength, splitmaxlength)
 
 
 def analyse(sample, fastq, srr, fasta, sizes, splitlength, splitminlength, splitmaxlength, threads=None):
@@ -77,11 +53,11 @@ def analyse(sample, fastq, srr, fasta, sizes, splitlength, splitminlength, split
     try:
         print ('Analyse sample {}'.format(sample))
         DownloadSample.download(sample, fastq, srr)
-        AlignSample.align(sample, fastq, fasta, threads)
-        FilterBam.filter_bam(sample, threads)
-        BamToBed.bam_to_bed(sample, threads)
+        RunBwa.align(sample, fastq, fasta, threads)
+        Bam2Bed.bam_to_bed(sample, threads)
         if splitlength is not None:
             SplitBed.split_bed(sample, splitlength, splitminlength, splitmaxlength)
+        PrepareGenomeCoverage.prepare_genome_coverage(sample, sizes)
         GenomeCoverage.genome_coverage(sample, sizes)
     except Exception as e:
         logging.exception('Could not analyse sample {}'.format(sample))
@@ -94,6 +70,7 @@ def analyse_merged(sample, sizes, splitlength, splitminlength, splitmaxlength):
     try:
         if splitlength is not None:
             SplitBed.split_bed(sample, splitlength, splitminlength, splitmaxlength)
+        PrepareGenomeCoverage.prepare_genome_coverage(sample, sizes)
         GenomeCoverage.genome_coverage(sample, sizes)
     except Exception as e:
         logging.exception('Could not analyse sample {}'.format(sample))
---
+++ mnaseseqtools/PrepareGenomeCoverage.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import subprocess
+
+import click
+import pandas as pd
+import seqtools.SplitBed
+
+BASE_SCALE = 1000000
+
+
+@click.command()
+@click.option('--samples', '-s', type=click.Path(exists=True), default='samples.txt',
+              help='Sample names listed one sample name by line.')
+@click.option('--sizes', '-S', type=click.Path(exists=True), default='sacCer3.chrom.sizes',
+              help='Size of chromosomes.')
+@click.option('--index', '-i', type=int, default=None,
+              help='Index of sample to process in samples file.')
+def main(samples, sizes, index):
+    '''Prepare BED file used for genome coverage on samples.'''
+    logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    sample_names = pd.read_csv(samples, header=None, sep='\t', comment='#')[0]
+    if index != None:
+        sample_names = [sample_names[index]]
+    for sample in sample_names:
+        prepare_genome_coverage(sample, sizes)
+
+
+def prepare_genome_coverage(sample, sizes):
+    '''Prepare BED file used for genome coverage on a single sample.'''
+    print ('Compute genome coverage on sample {}'.format(sample))
+    do_genome_coverage(sample, sizes)
+    splits = SplitBed.splits(sample)
+    for split in splits:
+        do_genome_coverage(split, sizes)
+
+
+def do_prepare_genome_coverage(sample, sizes):
+    bed_raw = sample + "-raw.bed"
+    bed_ignore_strand = sample + "-cov.bed"
+    center_annotations(bed_raw, bed_ignore_strand)
+
+
+def center_annotations(bed, output):
+    '''Resize annotations to 1 positioned at the center.'''
+    with open(bed, "r") as infile:
+        with open(output, "w") as outfile:
+            for line in infile:
+                if line.startswith('track') or line.startswith('browser') or line.startswith('#'):
+                    outfile.write(line)
+                    continue
+                columns = line.rstrip('\r\n').split('\t')
+                if len(columns) >= 3:
+                    start = int(columns[1])
+                    end = int(columns[2])
+                    length = end - start
+                    start = start + int(length / 2)
+                    end = start + 1
+                    outfile.write(columns[0])
+                    outfile.write("\t")
+                    outfile.write(str(start))
+                    outfile.write("\t")
+                    outfile.write(str(end))
+                    for i in range(3, len(columns)):
+                        outfile.write("\t")
+                        outfile.write(columns[i])
+                    outfile.write("\n")
+
+
+if __name__ == '__main__':
+    main()
