f2a7852448578920eaad6aa865a481b453398408
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-10-23 13:00:30 -0400

    Adds intersect scripts.

---
+++ bash/intersect-merge.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=def-robertf
+#SBATCH --time=24:00:00
+#SBATCH --array=0-0
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --mail-user=christian.poitras@ircm.qc.ca
+#SBATCH --mail-type=ALL
+
+if [ -z "$SLURM_ARRAY_TASK_ID" ]
+then
+  SLURM_ARRAY_TASK_ID=0
+fi
+
+intersectbed -s merge-filter.txt -a top10.txt $SLURM_ARRAY_TASK_ID
+split -s merge-filter.txt -i $SLURM_ARRAY_TASK_ID
+prepgenecov -s merge-filter.txt -i $SLURM_ARRAY_TASK_ID
+genecov -s merge-filter.txt -i $SLURM_ARRAY_TASK_ID
