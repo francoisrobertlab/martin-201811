2c92ecd24c1c484df87f5506e8f3cc8b3cb34b46
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-10-23 20:08:26 -0400

    Updates seqtools to 0.1.6.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'click>=7.0',
         'pandas>=0.25.0',
-        'seqtools@http://github.com/francoisrobertlab/seqtools/tarball/0.1.5'
+        'seqtools@http://github.com/francoisrobertlab/seqtools/tarball/0.1.6'
     ],
     entry_points={
         'console_scripts': [
