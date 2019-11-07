2aae6f92f9e06f7bbea77f347858b3ebd6e18d58
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-11-07 10:49:05 -0500

    Load plot2do module in mnase module.

--- mnase-201908.lua
+++ mnase-201908.lua
@@ -14,7 +14,8 @@ This module loads the following modules and their requirements:
     - samtools/1.9
     - bedtools/2.27.1
     - kentutils/20180716
-    - r/3.6.0
+    - vap
+    - plot2do
 ]])
 
 whatis("Version: 1.0.0")
@@ -30,8 +31,8 @@ always_load("bowtie2/2.3.4.3")
 always_load("samtools/1.9")
 always_load("bedtools/2.27.1")
 always_load("kentutils/20180716")
-always_load("r/3.6.0")
 always_load("vap")
+always_load("plot2do")
 
 local base = "~/projects/def-robertf/mnase-201908"
 prepend_path("PATH", pathJoin(base,"bash"))
