47b76028d391bbcf6ba0bf75403bab3631e43495
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-11-08 12:35:38 -0500

    Fixes memory usage in plot2DO bash script.

--- bash/plot2do.sh
+++ bash/plot2do.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --array=0-0
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=20G
+#SBATCH --mem=80G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
 
