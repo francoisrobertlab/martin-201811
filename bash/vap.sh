de15670fb04741e814b29d316fbf0214dd6a9a95
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-11-10 08:55:03 -0500

    Use array for vap bash script.

--- bash/vap.sh
+++ bash/vap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #SBATCH --account=def-robertf
 #SBATCH --time=24:00:00
-#SBATCH --ntasks=1
+#SBATCH --array=0-0
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
 
-runvap $@
+if [ -z "$SLURM_ARRAY_TASK_ID" ]
+then
+  SLURM_ARRAY_TASK_ID=0
+fi
+
+runvap -i $SLURM_ARRAY_TASK_ID $@
