61b189db149662918bfe01d4b1b9482e3dee9edc
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-12-14 07:42:57 -0500

    Fixes cpu and memory usage for download bash script.

--- bash/download.sh
+++ bash/download.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=def-robertf
 #SBATCH --time=24:00:00
 #SBATCH --array=0-0
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --output=download-%A_%a.out
