ddc3885a04e6e9781fa26db48e1447f7d1651f54
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-01-06 16:05:28 -0500

    Use lmfit to fit gaussian.

--- mnaseseqtools/DyadCoverageFitGaussian.py
+++ mnaseseqtools/DyadCoverageFitGaussian.py
@@ -1,7 +1,7 @@
 import logging
-from scipy.optimize import curve_fit
 
 import click
+from lmfit.models import GaussianModel, ConstantModel
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -11,43 +11,63 @@ import seqtools.SplitBed as sb
 @click.command()
 @click.option('--samples', '-s', type=click.Path(exists=True), default='samples.txt',
               help='Sample names listed one sample name by line.')
+@click.option('--components', '-c', is_flag=True,
+              help='Shows fit components and initial fit in plot.')
+@click.option('--verbose', '-v', is_flag=True,
+              help='Shows fit report.')
 @click.option('--index', '-i', type=int, default=None,
               help='Index of sample to process in samples file.')
-def main(samples, index):
+def main(samples, components, verbose, index):
     '''Fits gaussian curve to dyad coverage.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     sample_names = pd.read_csv(samples, header=None, sep='\t', comment='#')[0]
     if index != None:
         sample_names = [sample_names[index]]
     for sample in sample_names:
-        fit_gaussian(sample)
+        fit_gaussian(sample, components, verbose)
         splits = sb.splits(sample)
         for split in splits:
-            fit_gaussian(split)
+            fit_gaussian(split, components, verbose)
 
 
-def fit_gaussian(sample):
+def fit_gaussian(sample, components, verbose):
     '''Fits gaussian curve to dyad coverage for a single sample.'''
     print ('Fits gaussian curve to dyad coverage of sample {}'.format(sample))
     input = sample + '-dyad.txt'
-    dyads = pd.read_csv(input, sep='\t', comment='#')
+    dyads = pd.read_csv(input, sep='\t', index_col=0, comment='#')
+    x = dyads.index.values
+    y = dyads['Relative Frequency'].values
     plt.plot(dyads.index.values, dyads['Relative Frequency'].values, color='red')
     plt.xlabel('Position relative to dyad (bp)')
     plt.ylabel('Relative Frequency')
     plt.title(sample)
     plot_output = sample + '-dyad-gaussian.png'
     try:
-        init_vals = [dyads['Relative Frequency'].max(), 0.0, dyads.index.max() / 2]
-        best_vals, covar = curve_fit(gaussian, dyads.index.values, dyads['Relative Frequency'].values, p0=init_vals)
-        plt.plot(dyads.index.values, gaussian(dyads.index.values, *best_vals), color='grey')
-        plt.savefig(plot_output)
+        constant = ConstantModel(prefix='c_')
+        pars = constant.make_params()
+        pars['c_c'].set(value=dyads['Relative Frequency'].min(), min=0.0, max=dyads['Relative Frequency'].max())
+        gauss = GaussianModel(prefix='g_')
+        pars.update(gauss.make_params())
+        pars['g_center'].set(value=0.0)
+        pars['g_sigma'].set(value=dyads.index.max() / 2)
+        pars['g_amplitude'].set(value=dyads['Relative Frequency'].max() * 100)
+        mod = constant + gauss
+        init = mod.eval(pars, x=x)
+        out = mod.fit(y, pars, x=x)
+        if components:
+            plt.plot(x, init, 'b--', label='initial fit')
+        if verbose:
+            print(out.fit_report(min_correl=0.5))
+        plt.plot(x, out.best_fit, 'b-', label='best fit')
+        if components:
+            comps = out.eval_components(x=x)
+            plt.plot(x, np.repeat(comps['c_'], len(x)), 'g--', label='Constant component')
+            plt.plot(x, comps['g_'], 'm--', label='Gaussian component')
     except Exception as e:
         logging.warning('could not fit gaussian curve to sample {}'.format(sample), e)
+    plt.legend(loc='lower right')
+    plt.savefig(plot_output)
     plt.clf()
-
-
-def gaussian(x, amp, cen, wid):
-    return amp * np.exp(-(x - cen) ** 2 / (2.0 * wid ** 2.0))
 
 
 if __name__ == '__main__':
