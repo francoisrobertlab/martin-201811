289d3ca0096cdc8dec3a6459b3da15a4bc127775
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-01-22 13:05:41 -0500

    Improves parameter names.

--- mnaseseqtools/DyadCoverageFitDoubleGaussian.py
+++ mnaseseqtools/DyadCoverageFitDoubleGaussian.py
@@ -19,68 +19,68 @@ import seqtools.SplitBed as sb
               help='Save vectorial plot.')
 @click.option('--verbose', '-v', is_flag=True,
               help='Shows fit report.')
-@click.option('-c1', type=float, default=None,
+@click.option('--center1', '-c1', type=float, default=None,
               help='Center of first gaussian. Defaults to minus a quater of maximum index')
 @click.option('--cmin1', '-cm1', type=float, default=None,
               help='Minimum value for center of first gaussian. Defaults to unbounded')
 @click.option('--cmax1', '-cM1', type=float, default=None,
               help='Maximum value for center of first gaussian. Defaults to unbounded')
-@click.option('-a1', type=float, default=None,
-              help='Amplitude of first gaussian. Defaults to maximum relative frequency')
+@click.option('--amp1', '-a1', type=float, default=None,
+              help='Amplitude of first gaussian. Defaults to half of maximum relative frequency, approximately')
 @click.option('--amin1', '-am1', type=float, default=None,
               help='Minimum amplitude of first gaussian. Defaults to unbounded')
-@click.option('-s1', type=float, default=None,
+@click.option('--sigma1', '-s1', type=float, default=None,
               help='Width (sigma) of first gaussian. Defaults to a fifth of maximum index')
 @click.option('--smin1', '-sm1', type=float, default=None,
               help='Minimum width (sigma) of first gaussian. Defaults unbounded')
-@click.option('-c2', type=float, default=None,
+@click.option('--center2', '-c2', type=float, default=None,
               help='Center of second gaussian. Defaults to plus a quater of maximum index')
 @click.option('--cmin2', '-cm2', type=float, default=None,
               help='Minimum value for center of second gaussian. Defaults to unbounded')
 @click.option('--cmax2', '-cM2', type=float, default=None,
               help='Maximum value for center of second gaussian. Defaults to unbounded')
-@click.option('-a2', type=float, default=None,
-              help='Amplitude of second gaussian. Defaults to maximum relative frequency')
+@click.option('--amp2', '-a2', type=float, default=None,
+              help='Amplitude of second gaussian. Defaults to half of maximum relative frequency, approximately')
 @click.option('--amin2', '-am2', type=float, default=None,
               help='Minimum amplitude of second gaussian. Defaults to unbounded')
-@click.option('-s2', type=float, default=None,
+@click.option('--sigma2', '-s2', type=float, default=None,
               help='Width (sigma) of second gaussian. Defaults to a fifth of maximum index')
 @click.option('--smin2', '-sm2', type=float, default=None,
               help='Minimum width (sigma) of second gaussian. Defaults to unbounded')
 @click.option('--index', '-i', type=int, default=None,
               help='Index of sample to process in samples file.')
-def main(samples, components, gaussian, svg, verbose, c1, cmin1, cmax1, a1, amin1, s1, smin1, c2, cmin2, cmax2, a2, amin2, s2, smin2, index):
+def main(samples, components, gaussian, svg, verbose, center1, cmin1, cmax1, amp1, amin1, sigma1, smin1, center2, cmin2, cmax2, amp2, amin2, sigma2, smin2, index):
     '''Fits double gaussian curve to dyad coverage.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     sample_names = pd.read_csv(samples, header=None, sep='\t', comment='#')[0]
     if index != None:
         sample_names = [sample_names[index]]
     for sample in sample_names:
-        fit_double_gaussian(sample, components, gaussian, svg, verbose, c1, cmin1, cmax1, a1, amin1, s1, smin1, c2, cmin2, cmax2, a2, amin2, s2, smin2)
+        fit_double_gaussian(sample, components, gaussian, svg, verbose, center1, cmin1, cmax1, amp1, amin1, sigma1, smin1, center2, cmin2, cmax2, amp2, amin2, sigma2, smin2)
         splits = sb.splits(sample)
         for split in splits:
-            fit_double_gaussian(split, components, gaussian, svg, verbose, c1, cmin1, cmax1, a1, amin1, s1, smin1, c2, cmin2, cmax2, a2, amin2, s2, smin2)
+            fit_double_gaussian(split, components, gaussian, svg, verbose, center1, cmin1, cmax1, amp1, amin1, sigma1, smin1, center2, cmin2, cmax2, amp2, amin2, sigma2, smin2)
            
 
-def fit_double_gaussian(sample, components, gaussian, svg, verbose, c1, cmin1, cmax1, a1, amin1, s1, smin1, c2, cmin2, cmax2, a2, amin2, s2, smin2):
+def fit_double_gaussian(sample, components, gaussian, svg, verbose, center1, cmin1, cmax1, amp1, amin1, sigma1, smin1, center2, cmin2, cmax2, amp2, amin2, sigma2, smin2):
     '''Fits double gaussian curve to dyad coverage for a single sample.'''
     print ('Fits double gaussian curve to dyad coverage of sample {}'.format(sample))
     input = sample + '-dyad.txt'
     dyads = pd.read_csv(input, sep='\t', index_col=0, comment='#')
     x = dyads.index.values
     y = dyads['Relative Frequency'].values
-    if not a1:
-        a1 = dyads['Relative Frequency'].max() * 50
-    if not c1:
-        c1 = -dyads.index.max() / 4
-    if not s1:
-        s1 = dyads.index.max() / 5
-    if not a2:
-        a2 = dyads['Relative Frequency'].max() * 50
-    if not c2:
-        c2 = dyads.index.max() / 4
-    if not s2:
-        s2 = dyads.index.max() / 5
+    if not amp1:
+        amp1 = dyads['Relative Frequency'].max() * 50
+    if not center1:
+        center1 = -dyads.index.max() / 4
+    if not sigma1:
+        sigma1 = dyads.index.max() / 5
+    if not amp2:
+        amp2 = dyads['Relative Frequency'].max() * 50
+    if not center2:
+        center2 = dyads.index.max() / 4
+    if not sigma2:
+        sigma2 = dyads.index.max() / 5
     plt.figure()
     plt.title(sample)
     plt.xlabel('Position relative to dyad (bp)')
@@ -95,14 +95,14 @@ def fit_double_gaussian(sample, components, gaussian, svg, verbose, c1, cmin1, c
         pars['c_c'].set(value=dyads['Relative Frequency'].min(), min=0.0, max=dyads['Relative Frequency'].max())
         gauss1 = GaussianModel(prefix='g1_')
         pars.update(gauss1.make_params())
-        pars['g1_center'].set(value=c1, min=cmin1, max=cmax1)
-        pars['g1_sigma'].set(value=s1, min=smin1)
-        pars['g1_amplitude'].set(value=a1, min=amin1)
+        pars['g1_center'].set(value=center1, min=cmin1, max=cmax1)
+        pars['g1_sigma'].set(value=sigma1, min=smin1)
+        pars['g1_amplitude'].set(value=amp1, min=amin1)
         gauss2 = GaussianModel(prefix='g2_')
         pars.update(gauss2.make_params())
-        pars['g2_center'].set(value=c2, min=cmin2, max=cmax2)
-        pars['g2_sigma'].set(value=s2, min=smin2)
-        pars['g2_amplitude'].set(value=a2, min=amin2)
+        pars['g2_center'].set(value=center2, min=cmin2, max=cmax2)
+        pars['g2_sigma'].set(value=sigma2, min=smin2)
+        pars['g2_amplitude'].set(value=amp2, min=amin2)
         mod = constant + gauss1 + gauss2
         init = mod.eval(pars, x=x)
         out = mod.fit(y, pars, x=x)
