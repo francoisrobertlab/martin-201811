5706c3d2452f36c2f727316d1c0e004418e62d5c
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-01-22 13:06:04 -0500

    Adds parameters to control gaussian fit in DyadCoverageFitGaussian.

--- mnaseseqtools/DyadCoverageFitGaussian.py
+++ mnaseseqtools/DyadCoverageFitGaussian.py
@@ -17,28 +17,48 @@ import seqtools.SplitBed as sb
               help='Save vectorial plot.')
 @click.option('--verbose', '-v', is_flag=True,
               help='Shows fit report.')
+@click.option('--center', type=float, default=None,
+              help='Center of first gaussian. Defaults to 0')
+@click.option('--cmin', '-cm', type=float, default=None,
+              help='Minimum value for center of first gaussian. Defaults to unbounded')
+@click.option('--cmax', '-cM', type=float, default=None,
+              help='Maximum value for center of first gaussian. Defaults to unbounded')
+@click.option('--amp', type=float, default=None,
+              help='Amplitude of first gaussian. Defaults to maximum relative frequency, approximately')
+@click.option('--amin', '-am', type=float, default=None,
+              help='Minimum amplitude of first gaussian. Defaults to unbounded')
+@click.option('--sigma', type=float, default=None,
+              help='Width (sigma) of first gaussian. Defaults to half of maximum index')
+@click.option('--smin', '-sm', type=float, default=None,
+              help='Minimum width (sigma) of first gaussian. Defaults unbounded')
 @click.option('--index', '-i', type=int, default=None,
               help='Index of sample to process in samples file.')
-def main(samples, components, svg, verbose, index):
+def main(samples, components, svg, verbose, center, cmin, cmax, amp, amin, sigma, smin, index):
     '''Fits gaussian curve to dyad coverage.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     sample_names = pd.read_csv(samples, header=None, sep='\t', comment='#')[0]
     if index != None:
         sample_names = [sample_names[index]]
     for sample in sample_names:
-        fit_gaussian(sample, components, svg, verbose)
+        fit_gaussian(sample, components, svg, verbose, center, cmin, cmax, amp, amin, sigma, smin)
         splits = sb.splits(sample)
         for split in splits:
-            fit_gaussian(split, components, svg, verbose)
+            fit_gaussian(split, components, svg, verbose, center, cmin, cmax, amp, amin, sigma, smin)
 
 
-def fit_gaussian(sample, components, svg, verbose):
+def fit_gaussian(sample, components, svg, verbose, center, cmin, cmax, amp, amin, sigma, smin):
     '''Fits gaussian curve to dyad coverage for a single sample.'''
     print ('Fits gaussian curve to dyad coverage of sample {}'.format(sample))
     input = sample + '-dyad.txt'
     dyads = pd.read_csv(input, sep='\t', index_col=0, comment='#')
     x = dyads.index.values
     y = dyads['Relative Frequency'].values
+    if not amp:
+        amp = dyads['Relative Frequency'].max() * 100
+    if not center:
+        center = 0.0
+    if not sigma:
+        sigma = dyads.index.max() / 2
     plt.figure()
     plt.title(sample)
     plt.xlabel('Position relative to dyad (bp)')
@@ -53,9 +73,9 @@ def fit_gaussian(sample, components, svg, verbose):
         pars['c_c'].set(value=dyads['Relative Frequency'].min(), min=0.0, max=dyads['Relative Frequency'].max())
         gauss = GaussianModel(prefix='g_')
         pars.update(gauss.make_params())
-        pars['g_center'].set(value=0.0)
-        pars['g_sigma'].set(value=dyads.index.max() / 2)
-        pars['g_amplitude'].set(value=dyads['Relative Frequency'].max() * 100)
+        pars['g_center'].set(value=center, min=cmin, max=cmax)
+        pars['g_sigma'].set(value=sigma, min=smin)
+        pars['g_amplitude'].set(value=amp, min=amin)
         mod = constant + gauss
         init = mod.eval(pars, x=x)
         out = mod.fit(y, pars, x=x)
