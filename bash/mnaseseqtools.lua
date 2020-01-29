aa488349275e5b50060be5dd7d8ead0e0f2fb335
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-01-29 14:12:14 -0500

    Adds fastqc to compute canada module.

--- bash/mnaseseqtools.lua
+++ bash/mnaseseqtools.lua
@@ -10,6 +10,7 @@ This module sets the following environment variables:
 This module loads the following modules and their requirements:
     - python/3.7.4
     - perl/5.22.4
+    - fastqc/0.11.8
     - bwa/0.7.17
     - bowtie2/2.3.4.3
     - samtools/1.9
@@ -29,6 +30,7 @@ always_load("nixpkgs/16.09")
 always_load("gcc/7.3.0")
 always_load("python/3.7.4")
 always_load("perl/5.22.4")
+always_load("fastqc/0.11.8")
 always_load("bwa/0.7.17")
 always_load("bowtie2/2.3.4.3")
 always_load("samtools/1.9")
