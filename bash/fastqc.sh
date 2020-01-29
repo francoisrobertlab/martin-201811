99436a72492037e04c983c73bf10435fc19a3c76
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-01-29 14:12:30 -0500

    Adds statistics and fastqc bash scripts.

---
+++ bash/fastqc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-robertf
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-user=christian.poitras@ircm.qc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --output=fastqc-%A.out
+#SBATCH --error=fastqc-%A.out
+
+args=("$@")
+if [ ! -z "$SLURM_CPUS_PER_TASK" ]
+then
+  args+=("-t" "$SLURM_CPUS_PER_TASK")
+fi
+
+fastqc "${args[@]}"
